fa87f7129c99d28b95e72997d3066de33dd86b9e
Gabriela <gabrielapereirabarreto@gmail.com>
2022-09-11 16:36:08 -0300

    page objects

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress"/>
+import EnderecoPage from '../support/page-objects/endereco.page'
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
    beforeEach( () => {
@@ -9,8 +10,9 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     
    });
    
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
-       
-        //cadastro de endereço
+    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+       EnderecoPage.editarEnderecoFaturamento ('Gabriela', 'Barreto', 'Google', 'Brasil', 'Av. Brasil', '1100', 'São Paulo', 'São Paulo', '01000100', '11998988989', 'email@dominio.com')
+       cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+        
     });
 });
