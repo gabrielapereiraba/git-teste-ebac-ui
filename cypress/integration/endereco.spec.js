89597f77b5ae6b13c42727cc1c2214c3794aea93
Gabriela <gabrielapereirabarreto@gmail.com>
2022-09-11 17:05:19 -0300

    Massa de dados em lista

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress"/>
 import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require ('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
    beforeEach( () => {
@@ -10,9 +11,27 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     
    });
    
-    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
        EnderecoPage.editarEnderecoFaturamento ('Gabriela', 'Barreto', 'Google', 'Brasil', 'Av. Brasil', '1100', 'São Paulo', 'São Paulo', '01000100', '11998988989', 'email@dominio.com')
        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
         
     });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+      EnderecoPage.editarEnderecoFaturamento (
+         dadosEndereco[1].nome,
+         dadosEndereco[1].sobrenome,
+         dadosEndereco[1].empresa,
+         dadosEndereco[1].pais,
+         dadosEndereco[1].endereco,
+         dadosEndereco[1].numero,
+         dadosEndereco[1].cidade,
+         dadosEndereco[1].estado,
+         dadosEndereco[1].cep,
+         dadosEndereco[1].telefone,
+         dadosEndereco[1].email
+         )
+      cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+       
+   });
 });
